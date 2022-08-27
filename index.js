10e6a1049839a974c92d8427155b045766d58026
makhosi6 <ndondo330@gmail.com>
2022-08-27 19:12:52 +0200

    Saturday 08/27/2022 19:12 - IniTIal CoMMit

---
+++ index.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+const helpers = require('./store/helpers');
+//
+(async function() {
+    const browser = await puppeteer.launch({
+        args: helpers.argsArr,
+        defaultViewport: null,
+        headless: helpers.bool,
+        // executablePath: helpers.exPath
+    });
+ 
+    console.log({
+        "Browser Info =>": {
+            wsEndpoint: browser.wsEndpoint(),
+            version: await browser.version(),
+            userAgent: await browser.userAgent(),
+          
+        }
+    })
+    // module.exports = browser.wsEndpoint();
+})();
