d1ec8e55c786de038d25ee032a8bd80250b356e4
makhosi6 <ndondo330@gmail.com>
2023-06-03 16:49:28 +0200

    Sat  3 Jun 16:49:28 SAST 2023 - last commit from Zen

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const puppeteer = require('puppeteer');
-const helpers = require('./store/helpers');
+// const helpers = require('./store/helpers');
 //
 (async function() {
     const browser = await puppeteer.launch({
-        args: helpers.argsArr,
-        defaultViewport: null,
-        headless: helpers.bool,
+        args:  [
+    "--ignore-certificate-errors",
+    "--no-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-setuid-sandbox",
+    "--window-size=1920,1080",
+    "--disable-accelerated-2d-canvas",
+    "--disable-gpu",
+  ],
+userAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+       // defaultViewport: null,
+       // headless: helpers.bool,
         // executablePath: helpers.exPath
     });
  
