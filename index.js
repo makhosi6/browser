2b500c3ac0b23fee07f5ca05b284ccd35aa8e5ec
makhosi6 <ndondo330@gmail.com>
2023-06-30 17:56:05 +0200

    new functioanlity

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const puppeteer = require('puppeteer');
-// const helpers = require('./store/helpers');
+const puppeteer = require("puppeteer");
 //
-(async function() {
-    const browser = await puppeteer.launch({
-        args:  [
-    "--ignore-certificate-errors",
-    "--no-sandbox",
-    "--disable-dev-shm-usage",
-    "--disable-setuid-sandbox",
-    "--window-size=1920,1080",
-    "--disable-accelerated-2d-canvas",
-    "--disable-gpu",
-  ],
-userAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-       // defaultViewport: null,
-       // headless: helpers.bool,
-        // executablePath: helpers.exPath
-    });
- 
-    console.log({
-        "Browser Info =>": {
-            wsEndpoint: browser.wsEndpoint(),
-            version: await browser.version(),
-            userAgent: await browser.userAgent(),
-          
-        }
-    })
-    // module.exports = browser.wsEndpoint();
+(async function () {
+  const browser = await puppeteer.launch({
+    args: [
+      "--ignore-certificate-errors",
+      "--no-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--window-size=1920,1080",
+      "--disable-accelerated-2d-canvas",
+      "--disable-gpu",
+    ],
+    userAgent:
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+    defaultViewport: null,
+  });
+
+  console.log({
+    "Browser Info =>": {
+      date: new Date(),
+      wsEndpoint: browser.wsEndpoint(),
+      version: await browser.version(),
+      userAgent: await browser.userAgent(),
+    },
+  });
+  ///
+  await broadcastEndpoint(browser.wsEndpoint());
 })();
+
+async function broadcastEndpoint(wsEndpoint) {}
